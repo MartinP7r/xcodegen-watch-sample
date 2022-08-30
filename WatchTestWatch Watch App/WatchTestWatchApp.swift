83abff783a415e490d74a94729324b01466ad844
Martin Pfundmair <dev@martin.pfundmair.de>
2022-08-30 14:25:08 +0900

    Add watch and widget targets

---
+++ WatchTestWatch Watch App/WatchTestWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTestWatchApp.swift
+//  WatchTestWatch Watch App
+//
+//  Created by martin.p on 2022-08-30.
+//
+
+import SwiftUI
+
+@main
+struct WatchTestWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
