86eaf967d8c434bcc5ce82bc7d5ac9e6f0f9dc27
Martin Pfundmair <dev@martin.pfundmair.de>
2022-08-30 14:20:51 +0900

    Initial Commit

---
+++ WatchTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WatchTest
+//
+//  Created by martin.p on 2022-08-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchTest/WatchTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTestApp.swift
+//  WatchTest
+//
+//  Created by martin.p on 2022-08-30.
+//
+
+import SwiftUI
+
+@main
+struct WatchTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
